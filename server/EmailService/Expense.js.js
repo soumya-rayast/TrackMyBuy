53f866bd0ee34d699bf4d96b48216cbc3006c6b4
Soumya Rayast <113257526+soumya-rayast@users.noreply.github.com>
2024-07-28 22:23:55 +0530

    backend services done

---
+++ server/EmailService/Expense.js.js
@@ -0,0 +1,21 @@
+const dotenv = require("dotenv");
+const sendMail = require("../helpers/senmail")
+const Expense = require("../models/Expense");
+dotenv.config();
+
+const expenseEmail = async () => {
+    const expenses = await Expense.find();
+    const totalExpense = expenses.reduce(
+        (acc, expense) => acc + expense.value, 0
+    )
+    if (totalExpense > 1000) {
+        let messageOption = {
+            from: process.env.EMAIL,
+            to: process.env.ADMIN_EMAIL,
+            subject: "Warning",
+            text: `Your total expenses are ${total} , please review your expenses`,
+        }
+        await sendMail(messageOption);
+    }
+}
+module.exports = expenseEmail;
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const ExpenseRoute = require('./routes/expense')
 const cron = require("node-cron");
+const expenseEmail = require("./EmailService/Expense.js");
 dotenv.config();
 const app = express();
 
@@ -19,12 +20,15 @@ mongoose.connect(process.env.MONGO_URI).then(()=>{
     console.log('Database connected')
 }).catch((err)=>{
     console.log(err)
-})
+});
+
+// for sending mail
 const run =() =>{
     cron.schedule('* * * * *',()=>{
-        
+        expenseEmail();
     })
-}
+};
+
 run();
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running on port ${process.env.PORT}`)
